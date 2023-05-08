a67d17d03c89bd9e7f4743d31253e8d950478416
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-08 18:23:25 +0200

    completed conto

--- src/org/java/conto/Bank.java
+++ src/org/java/conto/Bank.java
@@ -1,9 +1,24 @@
 package org.java.conto;
 
+import java.util.Scanner;
+
 public class Bank {
 	public static void main(String[] args) {
-		Conto c = new Conto("Michele");
+		Scanner in = new Scanner(System.in);
 		
-		System.out.println(c);
+		System.out.println("Che metodo vuoi usare?");
+		String answer = in.nextLine();
+		if(answer.equals("versamento")) {
+			Conto c = new Conto("Michele", "versamento");
+			System.out.println(c);
+		} else if(answer.equals("prelievo")) {
+			Conto c = new Conto("Michele", "prelievo");
+			System.out.println(c);
+		} else {
+			System.out.println("Nessun metodo scelto");
+		}
+	
+		in.close();
+		
 	}
 }
--- src/org/java/conto/Conto.java
+++ src/org/java/conto/Conto.java
@@ -1,12 +1,24 @@
 package org.java.conto;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Conto {
-	private int conto = 88;
+	private int conto;
 	private String nome;
 	private double saldo;
 	
-	public Conto(String nome) {
+	public Conto(String nome, String type) {
+		Random rdm = new Random();
+		conto = rdm.nextInt(1000);
 		setNome(nome);
+		if(type.equals("versamento")) {
+			saldo = saldo + versamenti();
+		} else if( type.equals("prelievo")) {
+			saldo = prelievo();
+		}else {
+			System.out.println("Non hai scelto niente");;
+		}
 	}
 	public int getConto() {
 		return conto;
@@ -20,10 +32,28 @@ public class Conto {
 	public double getSaldo() {
 		return saldo;
 	}
+	public int versamenti() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Quanto vuoi versare?");
+		int soldi = in.nextInt();
+		in.close();
+		return soldi;
+	}
+	public int prelievo() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Quanto vuoi prelevare?");
+		int soldi = in.nextInt();
+		if(soldi > saldo) {
+			in.close();
+			return soldi;
+		}
+		in.close();
+		return soldi;
+	}
 	@Override
 	public String toString() {
 		return "conto: " + getConto()
 				+ " nome: " + getNome()
-				+ " saldo " + getSaldo();
+				+ " saldo: " + getSaldo();
 	}
 }
