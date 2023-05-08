0a777257feb4bf8a917b165d00a57fdea7127041
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-08 15:55:07 +0200

    completed + bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,7 +1,19 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
+		Random rdm = new Random();
 		
+		Prodotto p = new Prodotto(rdm.nextInt(100), "pippo", null, 0, 0);
+		
+		System.out.println("codice: " + p.getCod());
+		System.out.println("codice: " + p.padL());
+		System.out.println("nome: " + p.getNam());
+		System.out.println("descrizione: " + p.getDes());
+		System.out.println("prezzo: " + p.getPr());
+		System.out.println("iva: " + p.getIva());
+		System.out.println(p.toString());
 	}
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,58 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private double iva;
 	
+	public Prodotto(int codice, String nome, String descrizione, double prezzo, double iva){
+		setCod(codice);
+		setNam(nome);
+		setDes(descrizione);
+		setPr(prezzo);
+		setIva(iva);
+	}
+	public int getCod() {
+		return codice;
+	}
+	public void setCod(int codice) {
+		this.codice = codice; 
+	}
+	public String getNam() {
+		return nome;
+	}
+	public void setNam(String nome) {
+		this.nome = nome;
+	}
+	public String getDes() {
+		return descrizione;
+	}
+	public void setDes(String descrizione) {
+		this.descrizione = descrizione;
+	} 
+	public double getPr() {
+		return prezzo;
+	}
+	public void setPr(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public double getIva() {
+		return iva;
+	}
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	public String padL() {
+		return String.format("%10s", codice).replace(' ', '0');
+	}
+	@Override
+	public String toString() {
+		return "codice: " + codice 
+				+ " nome: " + nome 
+				+ " descrizione: " + descrizione 
+				+ " prezzo: " + prezzo 
+				+ " iva: " + iva;
+	}
 }
