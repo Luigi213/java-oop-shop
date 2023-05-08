29b647f9f415cc9d4118c67b026a4061e9f95fbb
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-08 15:58:25 +0200

    update

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -6,14 +6,14 @@ public class Main {
 	public static void main(String[] args) {
 		Random rdm = new Random();
 		
-		Prodotto p = new Prodotto(rdm.nextInt(100), "pippo", null, 0, 0);
+		Prodotto p = new Prodotto(rdm.nextInt(100), "Pizza", "Margherita", 18, 10);
 		
 		System.out.println("codice: " + p.getCod());
 		System.out.println("codice: " + p.padL());
 		System.out.println("nome: " + p.getNam());
 		System.out.println("descrizione: " + p.getDes());
 		System.out.println("prezzo: " + p.getPr());
-		System.out.println("iva: " + p.getIva());
+		System.out.println("iva: " + p.getIva() + "%");
 		System.out.println(p.toString());
 	}
 }
